565e2f452d5f7fdffd0c84cb314d15ddef5523bd
Yeison Suarez <yheison0035@gmail.com>
2025-07-17 10:35:45 -0500

    actualizacion de color

--- src/app/login.js
+++ src/app/login.js
@@ -15,7 +15,7 @@ export default function Login() {
   };
 
   return (
-    <div className="flex items-center justify-center min-h-screen bg-gray-100">
+    <div className="flex items-center justify-center min-h-screen bg-gray-200">
       <div className="w-full max-w-md bg-black rounded-2xl shadow-lg p-6 pb-9">
         <div className="flex flex-col items-center">
           <img
